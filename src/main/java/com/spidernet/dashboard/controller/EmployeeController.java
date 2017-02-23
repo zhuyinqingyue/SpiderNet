688629d02d4f9f8de69734eb7acc7031cc7c1072
Andy Yang <andy.yang@liferay.com>
2017-02-24 00:36:13 +0800

    Update EmployeeController.java

--- src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
+++ src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.spidernet.dashboard.entity.Employee;
 import com.spidernet.dashboard.service.EmployeeService;
 
