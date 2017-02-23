1aa4de16c8c09b3860e3efcd19c9ba1ec1c1151c
AndyYang78 <965998042@qq.com>
2017-02-24 00:15:02 +0800

    Test conflict

--- src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
+++ src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
@@ -17,7 +17,8 @@ import com.spidernet.dashboard.service.EmployeeService;
 
 @Controller
 @RequestMapping(value="/employee")
-public class EmployeeController {          
+public class EmployeeController {
+	
 	@Resource 
 	EmployeeService userService;       
 }
