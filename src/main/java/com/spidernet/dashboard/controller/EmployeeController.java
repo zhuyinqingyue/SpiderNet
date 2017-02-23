9648a44dab8ed8658736563cadfea097de8188b6
AndyYang78 <965998042@qq.com>
2017-02-23 23:21:40 +0800

    test

--- src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
+++ src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
@@ -16,14 +16,5 @@ import com.spidernet.dashboard.service.EmployeeService;
 @RequestMapping(value="/employee")
 public class EmployeeController {
 	@Resource 
-	EmployeeService userService;     
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	EmployeeService userService;
 }
