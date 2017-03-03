592d14dc38640860d32347773ea95959f1594fba
AndyYang78 <965998042@qq.com>
2017-03-03 12:33:39 +0800

    Source Format

--- src/main/webapp/js/load-trainning.js
+++ src/main/webapp/js/load-trainning.js
@@ -1,59 +1,58 @@
-$("#myCourse")
-		.click(
-				function(e) {
-					e.preventDefault();
+$("#myCourse").click(
+	function(e) {
+		e.preventDefault();
 
-					$.ajax({
-							url : "../../SpiderNet/service/trainningList",
-							type : "post",
-							async : true,
-							cache : false,
-							dataType : "json",
-							data : "capabilityId="
-									+ "5721b3b036f74b6c9441724e9f2bc063",
-							timeout : 20000,
-							success : function(trainningList) {
-								$("#trainningListTable tbody").remove();
-								var tbody = $("<tbody>");
-								tbody.appendTo($("#trainningListTable"));
+		$.ajax({
+				url : "../../SpiderNet/service/trainningList",
+				type : "post",
+				async : true,
+				cache : false,
+				dataType : "json",
+				data : "capabilityId="
+						+ "5721b3b036f74b6c9441724e9f2bc063",
+				timeout : 20000,
+				success : function(trainningList) {
+					$("#trainningListTable tbody").remove();
+					var tbody = $("<tbody>");
+					tbody.appendTo($("#trainningListTable"));
 
-								for (var i = 0; i < trainningList.length; i++) {
-									var tr = $("<tr></tr>");
-									tr.appendTo(tbody);
+					for (var i = 0; i < trainningList.length; i++) {
+						var tr = $("<tr></tr>");
+						tr.appendTo(tbody);
 
-									var td1 = $("<td> <input type='checkbox' id='subCheck' onclick='setSelectAll()'> </td>");
-									var td2 = $("<td>"
-											+ trainningList[i].courseName
-											+ "</td>");
-									var td3 = $("<td>"
-											+ trainningList[i].time
-											+ "</td>");
-									var td4 = $("<td>"
-											+ trainningList[i].location
-											+ "</td>");
-									var td5 = $("<td>"
-											+ trainningList[i].teacher
-											+ "</td>");
-									var td6 = $("<td>"
-											+ trainningList[i].url
-											+ "</td>");
-									var td7 = $("<td>"
-											+ trainningList[i].status
-											+ "</td>");
+						var td1 = $("<td> <input type='checkbox' id='subCheck' onclick='setSelectAll()'> </td>");
+						var td2 = $("<td>"
+								+ trainningList[i].courseName
+								+ "</td>");
+						var td3 = $("<td>"
+								+ trainningList[i].time
+								+ "</td>");
+						var td4 = $("<td>"
+								+ trainningList[i].location
+								+ "</td>");
+						var td5 = $("<td>"
+								+ trainningList[i].teacher
+								+ "</td>");
+						var td6 = $("<td>"
+								+ trainningList[i].url
+								+ "</td>");
+						var td7 = $("<td>"
+								+ trainningList[i].status
+								+ "</td>");
 
-									td1.appendTo(tr);
-									td2.appendTo(tr);
-									td3.appendTo(tr);
-									td4.appendTo(tr);
-									td5.appendTo(tr);
-									td6.appendTo(tr);
-									td7.appendTo(tr);
-								}
-								$("#trainningListTable").append("</tbdoy>");
-								$('#myModalClass').modal('show');
-							}
-						});
-			})
+						td1.appendTo(tr);
+						td2.appendTo(tr);
+						td3.appendTo(tr);
+						td4.appendTo(tr);
+						td5.appendTo(tr);
+						td6.appendTo(tr);
+						td7.appendTo(tr);
+					}
+					$("#trainningListTable").append("</tbdoy>");
+					$('#myModalClass').modal('show');
+				}
+			});
+})
 
 $("#selectAll").click(function() {
 	if ($("#selectAll").prop("checked")) {
