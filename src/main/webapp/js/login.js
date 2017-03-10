809077d27f32147c47853de6e256164de6fae76f
AndyYang78 <965998042@qq.com>
2017-03-10 14:38:08 +0800

    Update Login conponent

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $('#loginForm').bootstrapValidator({
+		message: 'This value is not valid',
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+        	userName: {
+				validators: {
+                    notEmpty: {
+                        message: '请输入您的用戶名'
+                    },
+
+                    regexp: {
+                        regexp: /^([\u4E00-\u9FA5]|\w)*$/,
+                        message: '请勿包含特殊字符'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 20,
+                        message: '请输入长度在1到20位之间的用戶名'
+                    },
+/*                     remote: {
+                        url: paths+'/service/employee/checkErExists',
+                        message: '用戶名不存在',
+                        delay :  2000,//per 2s send a request
+                        type: 'POST'
+                    }
+ */                }
+            },
+
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '请输入密碼'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 15,
+                        message: '请输入长度在1到15位密碼'
+                    },
+                }
+            }
+        }
+    }) .on('success.form.bv', function(e) {
+        // Prevent submit form
+        e.preventDefault();
+
+        var $form     = $(e.target);
+            validator = $form.data('bootstrapValidator');
+        if(validator){
+        	login(e.target);
+        }
+
+    }) ;
+});
+function login(e) {
+	var userName = $("#userName").val();
+	var password = $("#password").val();
+
+	$.ajax({
+			url : path+"/service/manage/login",
+			type : "post",
+			async : true,
+			cache : false,
+			dataType : "json",
+			data : {'userName' : userName, 'password' : password},
+			timeout : 20000,
+			success : function(result) {
+				if (result) {
+					 window.location.href = path+"/service/employee/index.html";
+				}
+				else {
+					$(".login-box").find('.alert').width(350);
+					$(".login-box").find('.alert').html('UserName or Password is invalid').show();
+				}
+			}
+		});
+}
