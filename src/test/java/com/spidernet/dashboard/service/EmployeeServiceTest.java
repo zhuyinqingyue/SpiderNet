2820b88686718d3be46e9d9f9883db85a974301c
AndyYang78 <965998042@qq.com>
2017-03-20 15:17:41 +0800

    Generate the test case for the service layer

---
+++ src/test/java/com/spidernet/dashboard/service/EmployeeServiceTest.java
@@ -0,0 +1,63 @@
+package com.spidernet.dashboard.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:conf/spring-mybatis.xml" })
+public class EmployeeServiceTest
+{
+
+    @Test
+    public void testAccountValid()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAccountValidByErNumber()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAccountValidByHrNumber()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchByErNumber()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchByHrNumber()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddEmployee()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCheckErExists()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCheckHrExists()
+    {
+        fail("Not yet implemented");
+    }
+
+}
