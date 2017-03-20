140dbd2c6028d76b82aac0a7fd06e2daacfd14e4
Hurricanedh <donghui_dh@163.com>
2017-03-20 15:08:48 +0800

    add pro junit test

---
+++ src/test/java/com/spidernet/dashboard/service/ProCapabilityServiceTest.java
@@ -0,0 +1,28 @@
+package com.spidernet.dashboard.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProCapabilityServiceTest
+{
+
+    @Test
+    public void testViewProCapability()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchProCapabilityByCapabilityId()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdateProCapability()
+    {
+        fail("Not yet implemented");
+    }
+
+}
