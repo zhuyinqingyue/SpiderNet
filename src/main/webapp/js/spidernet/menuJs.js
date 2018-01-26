6181b53973b40184d2838f1a6e8df8ebaa1bd7f9
43990496 <15389218162@163.com>
2018-01-26 10:37:25 +0800

    update menu css and form precheck

--- src/main/webapp/js/spidernet/menuJs.js
+++ src/main/webapp/js/spidernet/menuJs.js
@@ -15,15 +15,15 @@ var setting = {
    /*edit: {
         enable: true
     },*/
-    /*callback:{  
-        onCheck:onCheck  
-    } */
     callback: {
 		onClick: zTreeOnClick
 	}
 };
 
+var addNumber = 0;
+
 $(document).ready(function(){
+	$("#picUrl").iconPicker();
 	$.ajax({
 		url : path+"/service/menuInfo/showMenu",
 		type : "post",
@@ -39,6 +39,7 @@ $(document).ready(function(){
 		}
 	});
 	
+	
 	function cleanScreen(){
 		$("#pName").val($("#name").val());
 		$("#name").val("");
@@ -53,9 +54,6 @@ $(document).ready(function(){
 		var id = $("#id").val();
 		var name = $("#name").val();
 		var url = $("#menuUrl").val();
-//		var sort = $("#sort").val();
-//		alert($("#id").val());
-//		var parentId = $("#parentId").val();
 		var remark = $("#remark").val();
 		var picUrl = $("#picUrl").val();
 		var valid = 'Y';
@@ -69,87 +67,215 @@ $(document).ready(function(){
 			success:function(resultFlag){
 				if (resultFlag)
 	    		{
-	        		alert("成功!");
-	        		window.location.href = path+"/service/menuInfo/menuManagement.html";
+					cleanStyle();
+					var successAlert ="<div class=\"alert alert-success\">"
+						+"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+						+"<strong> Success !</strong> Add success please check on the left tree menu ."
+						+"</div>";
+					$("#alertInfo").append(successAlert);
+					setTimeout(function(){  
+						window.location.href = path+"/service/menuInfo/menuManagement.html";
+                     },3000);
 	    		}
 	        	else
 	    		{
-	        		alert("失败!");
+	        		var errorAlert ="<div class=\"alert alert-danger\">"
+		 		           +"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+		 		           +"<strong> Warning! </strong> Operation failure please from the new operation !"
+		 		           +"</div>";
+		        	$("#alertInfo").append(errorAlert);
 	    		}
 			}
 		})
 	};
 	
-	var num = 0;
+	
 	$("#add").click(function(){
-		if(num==0){
-			cleanScreen();
-			num++;
-		}else{
-			addMenuInfo();
-			num--;
+		var valuePre = preCheck('add');
+		if(valuePre){
+			if(addNumber==0){
+				cleanScreen();
+				addNumber++;
+			}else{
+				addMenuInfo();
+				addNumber--;
+			}
 		}
 		
 		
 	});
+	
+	
 
 	$("#update").click(function(){
-		var id = $("#id").val();
-		var name = $("#name").val();
-		var url = $("#menuUrl").val();
-		var sort = $("#sort").val();
-		var parentId = $("#parentId").val();
-		var remark = $("#remark").val();
-		var picUrl = $("#picUrl").val();
-		var valid = 'Y';
-
-		$.ajax({
-			url:path+"/service/menuInfo/updateMenu.shtml",
-			dataType:"json",
-			async:true,
-			data: {'id':id,'name':name,'url':url,'sort':sort,'parentId':parentId,'remark':remark,'picUrl':picUrl},
-			cache:false,
-			type:"post",
-			success:function(resultFlag){
-				if (resultFlag)
-	    		{
-	        		alert("成功!");
-	        		window.location.href = path+"/service/menuInfo/menuManagement.html";
-	    		}
-	        	else
-	    		{
-	        		alert("失败!");
-	    		}
-			}
-		});
+		var valuePre = preCheck('upd');
+		if(valuePre){
+			var id = $("#id").val();
+			var name = $("#name").val();
+			var url = $("#menuUrl").val();
+			var sort = $("#sort").val();
+			var parentId = $("#parentId").val();
+			var remark = $("#remark").val();
+			var picUrl = $("#picUrl").val();
+			var valid = 'Y';
+	
+			$.ajax({
+				url:path+"/service/menuInfo/updateMenu.shtml",
+				dataType:"json",
+				async:true,
+				data: {'id':id,'name':name,'url':url,'sort':sort,'parentId':parentId,'remark':remark,'picUrl':picUrl},
+				cache:false,
+				type:"post",
+				success:function(resultFlag){
+					if (resultFlag)
+		    		{
+						var successAlert ="<div class=\"alert alert-success\">"
+							+"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+							+"<strong> Success !</strong> Update success please check on the left tree menu ."
+							+"</div>";
+						$("#alertInfo").append(successAlert);
+		        		setTimeout(function(){  
+		        			window.location.href = path+"/service/menuInfo/menuManagement.html";
+                         },3000);
+		    		}
+		        	else
+		    		{
+		        		cleanStyle();
+		        		var errorAlert ="<div class=\"alert alert-danger\">"
+			 		           +"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+			 		           +"<strong> Warning! </strong> Operation failure please from the new operation !"
+			 		           +"</div>";
+			        	$("#alertInfo").append(errorAlert);
+		    		}
+				}
+			});
+		}
 	});
 	
 	$("#delete").click(function(){
+		var valuePre = preCheck('del');
+		if(valuePre){
+			var id = $("#id").val();
+			$.ajax({
+				url:path+"/service/menuInfo/deleteMenu",
+				dataType:"json",
+				async:true,
+				data:{"id":id},
+				cache:false,
+				type:"post",
+				success:function(resultFlag){
+					if (resultFlag)
+		    		{
+						cleanStyle();
+		        		var successAlert ="<div class=\"alert alert-success\">"
+			                   +"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+			                    +"<strong> Success !</strong> Delete success please check on the left tree menu ."
+			                    +"</div>";
+		        		$("#alertInfo").append(successAlert);
+		        		setTimeout(function(){  
+		        			window.location.href = path+"/service/menuInfo/menuManagement.html";
+                         },3000);
+		    		}
+		        	else
+		    		{
+		        		var errorAlert ="<div class=\"alert alert-danger\">"
+		 		           +"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+		 		           +"<strong> Warning! </strong> Operation failure please from the new operation !"
+		 		           +"</div>";
+		        		$("#alertInfo").append(errorAlert);
+		    		}
+				}
+			});
+		}
+	});
+	
+	function preCheck(type){
+		var preCheckValue = true;
+		var alertInfo = "";
 		var id = $("#id").val();
-		$.ajax({
-			url:path+"/service/menuInfo/deleteMenu",
-			dataType:"json",
-			async:true,
-			data:{"id":id},
-			cache:false,
-			type:"post",
-			success:function(resultFlag){
-				if (resultFlag)
-	    		{
-	        		alert("成功!");
-	        		window.location.href = path+"/service/menuInfo/menuManagement.html";
-	    		}
-	        	else
-	    		{
-	        		alert("失败!");
-	    		}
+		if(id==''){
+			preCheckValue = false;
+			alertInfo = " Please click on the tree menu on the left first and do it again! "
+		}else if(type!='del'){
+			var name = $("#name").val();
+			if(name == ''){
+				$("#name").parent().addClass("has-error");
+				$("#name").attr('placeholder','Input cannot be null !');
+				alertInfo = " Please input the correct information ."
+					preCheckValue = false;
 			}
-		});
+			var url = $("#menuUrl").val();
+			if(url == ''){
+				$("#menuUrl").parent().addClass("has-error");
+				$("#menuUrl").attr('placeholder','Input cannot be null !');
+				alertInfo = " Please input the correct information ."
+					preCheckValue = false;
+			}
+			var picUrl = $("#picUrl").val();
+			if(picUrl == ''){
+				$("#picUrl").parent().addClass("has-error");
+				$("#picUrl").attr('placeholder','Input cannot be null !');
+				alertInfo = " Please input the correct information ."
+					preCheckValue = false;
+			}
+		}
+		
+		if(!preCheckValue){
+			$("#alertInfo").empty();
+			var errorAlert ="<div class=\"alert alert-danger\">"
+		           +"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"
+		           +"<strong> Warning! </strong> "
+		           + alertInfo
+		           +"</div>";
+			$("#alertInfo").append(errorAlert);
+		}
+		return preCheckValue;
+	};
+	
+	
+	$(".form-control").blur(function(){
+		if($(this).val()!=''){
+			$(this).parent().removeClass("has-error");
+			$(this).parent().addClass("has-success");
+		}
 	});
 	
 	
-	
 });
+
+function cleanStyle(){
+	addNumber = 0;
+	$("#alertInfo").empty();
+	$('#pName').attr("disabled",false);
+	$("#name").parent().removeClass("has-error");
+	$("#menuUrl").parent().removeClass("has-error");
+	$("#picUrl").parent().removeClass("has-error");
+}
+
+function zTreeOnClick(event, treeId, treeNode) {
+//    alert(treeNode.tId + ", " + treeNode.name  +"," +treeNode.id);
+    $.ajax({
+		url:path+"/service/menuInfo/getMenuById",
+		dataType:"json",
+		async:true,
+		data:{"id":treeNode.id},
+		cache:false,
+		type:"post",
+		success:function(menu){
+			$("#id").val(menu.id);
+			$("#name").val(menu.name);
+			$("#menuUrl").val(menu.url);
+			$("#sort").val(menu.sort);
+			$("#parentId").val(menu.parentId);
+			$("#remark").val(menu.remark);
+			$("#picUrl").val(menu.picUrl);
+			$("#pName").val(menu.parentName);
+			cleanStyle();
+		}
+	});
+};
+
+
 
 var newCount = 1;
 function addHoverDom(treeId, treeNode) {
@@ -184,27 +310,7 @@ function removeHoverDom(treeId, treeNode) {
  };*/
 //$("#form-control").onKeyUp
 
-function zTreeOnClick(event, treeId, treeNode) {
-//    alert(treeNode.tId + ", " + treeNode.name  +"," +treeNode.id);
-    $.ajax({
-		url:path+"/service/menuInfo/getMenuById",
-		dataType:"json",
-		async:true,
-		data:{"id":treeNode.id},
-		cache:false,
-		type:"post",
-		success:function(menu){
-			$("#id").val(menu.id);
-			$("#name").val(menu.name);
-			$("#menuUrl").val(menu.url);
-			$("#sort").val(menu.sort);
-			$("#parentId").val(menu.parentId);
-			$("#remark").val(menu.remark);
-			$("#picUrl").val(menu.picUrl);
-			$("#pName").val(menu.parentName);
-		}
-	});
-};
+
 
 
 
