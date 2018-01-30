03430f3156123b5e67a35d80f385283a49ab682b
43990496 <15389218162@163.com>
2018-01-30 16:31:45 +0800

    Menu permission
    Change-Id: Id2f9bd4457db2fa8f716a280e7bd0c4f986f3580

--- src/main/webapp/js/spidernet/menuJs.js
+++ src/main/webapp/js/spidernet/menuJs.js
@@ -30,12 +30,14 @@ $(document).ready(function(){
 		async : true,
 		cache : false,
 		dataType : "json",
-//		data : {'userName' : userName, 'password' : password},
 		timeout : 20000,
 		success : function(jsonData) {
-//			var zNodes = JSON.stringify(jsonData);
 			$.fn.zTree.init($("#treeDemo"), setting, jsonData);
-//			$.fn.zTree.init($("#treeDemo"), setting, zNodes);
+			
+			$(function(){
+				var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+				treeObj.expandAll(true);
+				});
 		}
 	});
 	
@@ -195,8 +197,13 @@ $(document).ready(function(){
 		var id = $("#id").val();
 		if(id==''){
 			preCheckValue = false;
-			alertInfo = " Please click on the tree menu on the left first and do it again! "
-		}else if(type!='del'){
+			alertInfo = " Please click on the tree menu on the left first and try it again! "
+		}
+		else if(type == 'del' && $("#name").val() == ''){
+			preCheckValue = false;
+			alertInfo = " Please click on the tree menu on the left first and try it again! "
+		}
+		else if(type!='del'){
 			var name = $("#name").val();
 			if(name == ''){
 				$("#name").parent().addClass("has-error");
@@ -246,15 +253,14 @@ $(document).ready(function(){
 function cleanStyle(){
 	addNumber = 0;
 	$("#alertInfo").empty();
-	$('#pName').attr("disabled",false);
+	$('#pName').attr("disabled",true);
 	$("#name").parent().removeClass("has-error");
 	$("#menuUrl").parent().removeClass("has-error");
 	$("#picUrl").parent().removeClass("has-error");
 }
 
 function zTreeOnClick(event, treeId, treeNode) {
-//    alert(treeNode.tId + ", " + treeNode.name  +"," +treeNode.id);
-    $.ajax({
+	$.ajax({
 		url:path+"/service/menuInfo/getMenuById",
 		dataType:"json",
 		async:true,
@@ -274,47 +280,3 @@ function zTreeOnClick(event, treeId, treeNode) {
 		}
 	});
 };
-
-
-
-var newCount = 1;
-function addHoverDom(treeId, treeNode) {
-    var sObj = $("#" + treeNode.tId + "_span");
-    if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
-    var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
-        + "' title='add node' onfocus='this.blur();'></span>";
-    sObj.after(addStr);
-    var btn = $("#addBtn_"+treeNode.tId);
-    if (btn) btn.bind("click", function(){
-        var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-        zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (newCount++)});
-        return false;
-    });
-};
-
-function removeHoverDom(treeId, treeNode) {
-    $("#addBtn_"+treeNode.tId).unbind().remove();
-};
-
-//获取所有的选中的NODE Id
-/*function onCheck(e,treeId,treeNode){
-    var treeObj=$.fn.zTree.getZTreeObj("treeDemo"),
-    nodes=treeObj.getCheckedNodes(true),
-    v="";
-    for(var i=0;i<nodes.length;i++){
-    v+=nodes[i].name + ",";
-    alert(nodes[i].id); //获取选中节点的值
-    }
-    
-    
- };*/
-//$("#form-control").onKeyUp
-
-
-
-
-
-
-
-
-
